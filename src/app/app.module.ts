a42956ec97cfca6798c242e9c9f437422e57a7b0
Emmanuel John <emmanuelhashy@gmail.com>
2022-12-05 23:16:33 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RoutesModule } from './routes/routes/routes.module';
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/components/Quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
